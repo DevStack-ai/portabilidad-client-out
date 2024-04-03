b2fe30be6ba9cb5187a5e12b8c44d12526142c16
Juan Castillo <juanpablodlcc@gmail.com>
2024-04-02 19:37:16 -0600

    [REF] server file

--- server.cjs
+++ server.cjs
@@ -1,31 +1,22 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs')
-// const https = require('https')
-const app = express();
 const logger = require('morgan');
+const app = express();
 
 app.use(logger('common', {
   stream: fs.createWriteStream('./access.log', { flags: 'a' })
 }));
+
 app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, 'dist'), {
-  dotfiles: 'allow',
-  setHeaders: function (res, path) {
-    res.set("X-Xss-Protection", "1; mode=block ");
-    res.set("X-Frame-Options", "DENY");
-    res.set("X-Content-Type-Options", "nosniff");
-    res.set("Strict-Transport-Security", "max-age=31536000");
-    res.set("Referrer-Policy", "no-referrer");
-    res.set("Permissions-Policy", "camera=*; fullscreen=()");
-  }
-}));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
 });
-
